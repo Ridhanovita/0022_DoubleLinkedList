7fe62d806552ff7a6126f22fb1487785c9324cb5
Ridhanovita <novitaridha796@gmail.com>
2025-05-15 12:15:48 +0700

    step 8

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -56,7 +56,23 @@ class DoubleLinkedList
                 //step 7: START = newNode
                 START = newNode;
                 return;
- 
+            }
+
+            //Insert in between node
+            //step 8: Locate position for insertion
+            Node *current = START;
+            while (current -> next != NULL && current -> next -> noMhs < nim)
+            {
+                current = current->next;
+            }
+
+            if (current->next != NULL && nim == current->next->noMhs)
+            {
+                cout << "\nDuplicate roll number not allowed" << endl;
+                return;
+            }
+            
+
 
  
 
