32e5652ec25ee747bf2397a7181cc706ee527fa5
Ridhanovita <novitaridha796@gmail.com>
2025-05-15 12:20:53 +0700

    menambahkan fungsi searchdata

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -183,6 +183,37 @@ class DoubleLinkedList
             }
         }
 
+         void searchData()
+        {
+            if (START == NULL)
+            {
+                cout << "\nList is empty" << endl;
+                return;
+            }
+
+            int rollNo;
+            cout << "\nEnter the roll number to search: ";
+            cin >> rollNo;
+
+            Node *current = START;
+
+            //Step 1: Traverse to find matching roll number
+            while (current != NULL && current->noMhs != rollNo)
+                current = current->next;
+
+            //Step 2: Output result
+            if (current == NULL)
+            {
+                cout << "Record not found\n";
+            }
+            else
+            {
+                cout << "Record found\n";
+                cout << "Roll NUmber: " << current->noMhs << endl;
+            }
+        }
+};
+
 
 
 
