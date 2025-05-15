4b0899fc88eda92c5111d9cd5ae8f234c77aff89
Ridhanovita <novitaridha796@gmail.com>
2025-05-15 12:09:10 +0700

    menambah class Double linked list

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -5,7 +5,26 @@ using namespace std;
 class Node
 {
     public:
-    int noMhs;
-    Node *next;
-    Node *prev;
+        int noMhs;
+        Node *next;
+        Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+        Node *START;
+
+    public:
+        DoubleLinkedList()
+        {
+            START = NULL;
+        }
+        
+        void addNode()
+        {
+            int nim;
+            string nm;
+            cout << "\nEnter the roll number of the student";
+            cin >> nim;
+                 
