c1420287ca8f4194d5f1b77ba34976f82b2686b8
Ridhanovita <novitaridha796@gmail.com>
2025-05-15 12:12:34 +0700

    step 3

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -34,5 +34,15 @@ class DoubleLinkedList
             //step 2: Assign value to the data fields
             newNode -> noMhs = nim;
 
+            //step 3: Insert at beginning if the list is empty or nim is smallest
+            if (START == NULL || nim <= START ->noMhs)
+            {
+                if (START != NULL && nim == START -> noMhs)
+                {
+                    cout << "\nDuplicate number not allowed" << endl;
+                    return;
+                }
+ 
+
 
            
